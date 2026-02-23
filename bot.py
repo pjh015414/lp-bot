ad88a7f9b9d2ccfb0a2cda6eff3bfa0108fa9d24
pjh015414 <pjh015414@gmail.com>
2026-02-23 19:59:24 +0900

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands
 import requests
 
-DISCOGS_TOKEN = "DISCOGS_TOKEND"
-DISCORD_TOKEN = "DISCORD_TOKEN"
+DISCOGS_TOKEN = os.getenv("DISCOGS_TOKEN")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
